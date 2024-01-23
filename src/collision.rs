4f70d6c763854dca1e3324b27d1cb7c5fc6e87f4
George Mclachlan <georgemclachlan2@gmail.com>
2024-01-23 14:16:02 +1300

    Fix ball angle and speed calculations

--- src/collision.rs
+++ src/collision.rs
@@ -41,44 +41,41 @@ pub fn check_colliders(
 
             // reflect velocity on the x-axis if we hit something on the x-axis
             if reflect_x {
-                ball_velocity.direction.x = 1.0;
-                ball_velocity.direction.y = 0.0;
-
                 // Get distance between ball center and paddle center
                 let distance_to_center = ball_transform.translation.y - transform.translation.y;
 
                 // Normalise distance to [-1, 1] based on paddle height
-                let normalised_distance_to_center =
-                    distance_to_center / (crate::PADDLE_SCALE.y / 2.0);
+                let normalised_distance_to_center = f32::clamp(
+                    distance_to_center / (crate::PADDLE_SCALE.y / 2.0),
+                    -1.0,
+                    1.0,
+                );
 
-                // Adjust the angle based on the
-                let bounce_angle = normalised_distance_to_center
-                    * f32::to_radians(crate::BALL_MAX_BOUNCE_ANGLE_DEGREES);
+                // Adjust the angle based on the normalised
+                let mut bounce_angle =
+                    normalised_distance_to_center * crate::BALL_MAX_BOUNCE_ANGLE_DEGREES;
 
-                let sin = f32::sin(bounce_angle);
-                let cos = f32::cos(bounce_angle);
+                let mut base_direction: Vec2 = Vec2::new(1.0, 0.0);
 
-                // Calculate the new direction vector
-                ball_velocity.direction.x =
-                    ball_velocity.direction.x * cos - ball_velocity.direction.y * sin;
-
-                ball_velocity.direction.y =
-                    ball_velocity.direction.x * sin + ball_velocity.direction.y * sin;
-
-                // Flip x value if we are bouncing off left side
                 if collision == Collision::Left {
-                    ball_velocity.direction.x *= -1.0;
+                    bounce_angle *= -1.0;
+                    base_direction.x *= -1.0;
                 }
 
-                let normalied_direction: Vec2 = ball_velocity.direction;
+                let rotation_matrix = Mat2::from_angle(bounce_angle.to_radians());
+
+                // Calculate the new direction vector
+                // ball_velocity.direction = rotation_matrix * base_direction;
+                ball_velocity.direction = rotation_matrix.mul_vec2(base_direction);
 
                 // Add ball speed to normalied direction vector
                 ball_velocity.direction.x *= crate::BALL_SPEED;
-                ball_velocity.direction.y *= crate::BALL_SPEED
-                    * (f32::abs(normalied_direction.x) / f32::abs(normalied_direction.y));
+                ball_velocity.direction.y *=
+                    crate::BALL_SPEED / f32::cos(bounce_angle.abs().to_radians());
             }
 
             // reflect velocity on the y-axis if we hit something on the y-axis
+            // ignore y velocity if hitting corner of paddle
             if reflect_y {
                 ball_velocity.direction.y = -ball_velocity.direction.y;
             }
